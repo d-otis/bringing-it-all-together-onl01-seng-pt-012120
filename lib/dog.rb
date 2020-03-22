f8e76e1fb06d4f41861e75e3154925ca2a53d6e8
Dan Foley <dan@dan-foley.com>
2020-03-22 20:06:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,6 +72,7 @@ class Dog
     SQL
     dog = DB[:conn].execute(sql, name, breed)
     if !dog.empty?
+      binding.pry
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], dog_data[2])
     else
