27e94f7e225658c0258edafa926f6e809fe5afd2
Dan Foley <dan@dan-foley.com>
2020-03-22 20:09:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -81,7 +81,7 @@ class Dog
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
     binding.pry
-    DB[:conn].execute(sql, name)[0]
+    found_dog = DB[:conn].execute(sql, name)[0]
   end
 
 end
